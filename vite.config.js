30181df9ec1ceea82a45d1717fd8cc1790a6977a
marktim24 <timoshevich199524@mail.ru>
2023-04-20 20:07:05 +0300

    Vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	base:
+		process.env.NODE_ENV === 'production' ? '/age-calculator-app-main/' : '/',
+	plugins: [react()]
 })
